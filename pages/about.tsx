2665608d7eef40947dbe0f1fd2283245d334f1c7
Loren <mwl@live.com>
2021-01-12 16:49:22 +0800

    更新less

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import css from './index.less'
 
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
